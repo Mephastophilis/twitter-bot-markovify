11a7b9c143c0295d09a42ea30f5877ccfb2f7ced
Bryan Quigley <bquigley@tulane.edu>
2018-07-17 09:54:34 -0500

    advanced markov chain model B added

--- twitter_bot_markovify.py
+++ twitter_bot_markovify.py
@@ -10,6 +10,8 @@ import re
 import random
 import time
 import markovify
+import numpy as np
+import os
 
 credentials=[]
 
@@ -41,6 +43,7 @@ def textproduce():
             tweettext = re.sub('&amp', '', tweettext)
             tweettext = re.sub('\n',' ',tweettext)
             fileoutput.write(tweettext + ' ')
+    print("Text procudce completed")
     fileoutput.close
 
 def randomtweet():
@@ -58,12 +61,78 @@ def randomtweet():
             pass
     api.update_status(newtweet)
 
+
+def textproduce_b():
+    checkpoint = 0
+    while checkpoint == 0:
+        fileoutput_a = open("time_line_a.txt", "w")
+        fileoutput_b = open("time_line_b.txt", "w")
+
+        for friend in user.friends():
+            
+
+            text_a = np.random.randint(2)
+            if text_a == 1:
+                print(friend.screen_name + ' used in corpus A.')
+                for tweet in api.user_timeline(friend.screen_name, tweet_mode='extended', count = 30, include_rts = True):
+                    tweettext = re.sub('https:\/\/.*', '', tweet.full_text.encode('ascii','ignore'))
+                    tweettext = re.sub('&amp', '', tweettext)
+                    tweettext = re.sub('\n',' ',tweettext)
+                    fileoutput_a.write(tweettext + ' ')
+
+            else:
+                print(friend.screen_name + ' used in corpus B.')
+                for tweet in api.user_timeline(friend.screen_name, tweet_mode='extended', count = 30, include_rts = True):
+                    tweettext = re.sub('https:\/\/.*', '', tweet.full_text.encode('ascii','ignore'))
+                    tweettext = re.sub('&amp', '', tweettext)
+                    tweettext = re.sub('\n',' ',tweettext)
+                    fileoutput_b.write(tweettext + ' ')     
+        fileoutput_a.close
+        fileoutput_b.close   
+        
+        if os.stat("time_line_a.txt").st_size == 0 or os.stat("time_line_b.txt").st_size == 0:
+            open('time_line_a.txt', 'w').close()
+            open('time_line_b.txt', 'w').close()
+            print("One text file empty, redoing text distribution.")
+        else:
+            checkpoint = 1     
+
+    
+
+
+
+def randomtweet_b():
+
+    file_a = open("time_line_a.txt", "r")
+    file_b = open("time_line_b.txt", "r")
+    corpus_a = file_a.read()
+    corpus_b = file_b.read()
+    file_a.close
+    file_b.close
+
+    model_a = markovify.Text(corpus_a)
+    model_b = markovify.Text(corpus_b)
+    model_combo = markovify.combine([ model_a, model_b ], [ 1, 1 ])
+    newtweet = model_combo.make_short_sentence(random.randint(100,260))
+    print(newtweet)
+    for status in tweepy.Cursor(api.user_timeline).items():
+        if newtweet == status.text:
+            api.destroy_status(status.id)
+            print('duplicate tweet destroyed')
+        else:
+            pass
+    api.update_status(newtweet)
+
 tweetcount=0
 while True:
     textproduce()
+    textproduce_b()
     for i in range(5):
         randomtweet()
         tweetcount=tweetcount+1
-        print('tweet count = ' + str(tweetcount))
-        time.sleep(random.randint(40,300))
+        print('tweet count = ' + str(tweetcount) + '. Model A used.')
+        randomtweet_b()
+        tweetcount=tweetcount+1
+        print('tweet count = ' + str(tweetcount) + '. Model B used.')
+        time.sleep(random.randint(40,100))
     time.sleep(300)
